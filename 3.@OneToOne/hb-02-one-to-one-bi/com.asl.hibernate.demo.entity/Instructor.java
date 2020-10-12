78fd4ca9c6100f8671ca6700bf023eea61b23056
souvik2805 <57298478+souvik2805@users.noreply.github.com>
2020-10-13 01:56:04 +0530

    Add files via upload

---
+++ 3.@OneToOne/hb-02-one-to-one-bi/com.asl.hibernate.demo.entity/Instructor.java
@@ -0,0 +1,100 @@
+package com.asl.hibernate.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+//1. annotate the class as an entity and map to db table
+//2. define the fields
+//3. annotate the fields with db columns names
+// *** SET UP MAPPING to InstructorDetail Entity ****
+//4. create constructor
+//5. generate getter/setter method
+//6. generate toStrinng method (for debug)
+
+@Entity
+@Table(name="Instructor")
+public class Instructor {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="instructor_details_id")
+	private InstructorDetail instructorDetail;
+
+	public Instructor() {
+		System.out.println("\n\n Instructor  Entity called - 2- \n\n");
+	}
+
+	public Instructor(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetail getInstructorDetail() {
+		return instructorDetail;
+	}
+
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
+	
+}
